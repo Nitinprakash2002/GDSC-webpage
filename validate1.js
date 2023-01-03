cb567d134fe8dbb253b9d2d68126901b19637f18
NITIN PRAKASH <87832961+Nitinprakash2002@users.noreply.github.com>
2023-01-03 15:05:35 +0530

    Add files via upload

---
+++ validate1.js
@@ -0,0 +1,22 @@
+function validate()
+{
+    var get1=document.getElementById("inp1").value;
+    var get2=document.getElementById("inp2").value;
+    if(get1=="" || get2=="")
+    {
+        alert("Any one of the fields are Empty\nPlease Fill them immediately");
+    }
+    else if(get1=="gdsc1234@gmail.com" && get2=="nitin")
+    {
+        alert("You have Entered Correct Credential\n Logged In.");
+    }
+    else if(get1!="gdsc1234@gmail.com" || get2!="nitin")
+    {
+        alert("You have entered wrong credential\n Please provide correct credentail.");
+    }
+}
+function jump()
+{
+    window.location="join.html";
+    return false;
+}
